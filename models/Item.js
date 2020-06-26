53af3ad55239d48a631b6f4a891c1e774136031d
Alan Lee <alanleeone@gmail.com>
2020-06-26 14:33:27 -0700

    working on item schema

--- models/Item.js
+++ models/Item.js
@@ -3,7 +3,22 @@ const { model, Schema } = require('mongoose')
 const Item = new Schema({
   title: String,
   description: String,
+  category: {
+    type: String,
+    default: 'Miscellaneous'
+  },
+  keywords: [{
+    type: String,
+    default: ''
+  }],
+  photo: [{
+    imagePaths: String
+  }],
   isBought: {
+    type: Boolean,
+    default: false
+  },
+  live: {
     type: Boolean,
     default: false
   }
