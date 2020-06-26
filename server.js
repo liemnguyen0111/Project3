a0cbffa460ebf8047da00d5149d15af2ce5af016
Liem Nguyen <liemnguyen0111@gmail.com>
2020-06-26 11:03:56 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -7,8 +7,14 @@ const { Strategy : JWTStrategy, ExtractJwt } = require('passport-jwt')
 
 const app = express()
 
+app.use(express.static(join(__dirname, 'client', 'build')))
 app.use(express.urlencoded({ extended : true }))
 app.use(express.json())
+
+app.get('*', (req,res) => 
+{
+    res.sendfile(join(__dirname, 'client' , 'build' , 'index.html'))
+})
 
 require('./config')
 .then(() => app.listen(3001))
