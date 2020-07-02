42d902f2e07eccb1def0a4c5a37f4c0e95d8b353
Alan Lee <alanleeone@gmail.com>
2020-07-02 14:46:44 -0700

    created logic for user auth and display in jumbot

--- client/src/utils/UserAPI/UserAPI.js
+++ client/src/utils/UserAPI/UserAPI.js
@@ -3,8 +3,9 @@ import axios from "axios";
 const UserAPI = {
   createUser: (user) => axios.post("/api/users/register", user),
   loginUser: (user) => axios.post('api/users/login', user),
-  isLoggedIn: false,
-  authorizeUser: (header) => axios.get('/api/users/authorize', header),
+  authorizeUser: () => axios.get('/api/users/authorize',  {headers: {
+      'Authorization': `Bearer ${localStorage.getItem("user")}`,
+    }}),
   // getUser: () => axios.get("/api/user/:id"),
   // deleteUser: (id) => axios.delete(`/api/users/${id}`),
   // updateUser: (id, updates) => axios.put(`/api/users/${id}`, updates)
