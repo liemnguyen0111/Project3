2d369017285e51fa65d5105edd46b8473f836e5a
Jordan M <jordan.muliaman@gmail.com>
2020-07-01 21:05:46 -0700

    make title sticky

--- client/src/components/ItemLeftWindow/ItemLeftWindow.js
+++ client/src/components/ItemLeftWindow/ItemLeftWindow.js
@@ -3,26 +3,22 @@ import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import DetailsDialog from './DetailsDialog'
+import ItemImage from './ItemImage'
+
 
 const defaultProps = {
   bgcolor: 'background.paper',
   // m: 1,
   border: 1,
- 
-  style: { width: '100%', height: '600px'},
+  style: { width: '100%', height: '100%'},
 };
 
 const useStyles = makeStyles((theme) => ({
-  thumbnail: {
-    marginTop: '50px',
-    height: '50%',
-    width: '95%',
-    objectFit: 'contain',
-  },
   title: {
-    marginTop: '50px',
-    color: 'black',
+    marginTop: '10px',
+    color: '#616161',
   },
 }));
 
@@ -32,13 +28,15 @@ const ItemLeftWindow = () => {
   return (
     <div>
       <Box textAlign="center" borderColor="text.primary" {...defaultProps}>
-        <img className={classes.thumbnail} src="https://image.goat.com/crop/750/attachments/product_template_pictures/images/037/815/978/original/551059_00.png.png" alt=""/>
-        <Typography className={classes.title}>
-          <strong>Air Jordan 5 Retro 'Top 3'</strong>
-        </Typography>
-        <Button>
-          <DetailsDialog />
-        </Button>
+        <ItemImage />
+        <Grid item xs={12} style={{ position: 'sticky', bottom: '0'}}>
+          <Typography className={classes.title}>
+            <strong>Air Jordan 5 Retro 'Top 3'</strong>
+          </Typography>
+          <Button>
+            <DetailsDialog />
+          </Button>
+        </Grid>
       </Box>
     </div>
   )
